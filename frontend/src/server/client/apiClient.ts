bfcd907bb11a142bffd972934f7519f68d287f0c
Justine Okumu <okumujustine01@gmail.com>
2025-01-21 13:29:29 -0600

    base url based on docker or local dev

--- frontend/src/server/client/apiClient.ts
+++ frontend/src/server/client/apiClient.ts
@@ -1,7 +1,11 @@
 import axios from 'axios';
 
+const isRunningInDocker = () => {
+    return process.env.RUNNING_IN_DOCKER === 'true';
+};
+
 export const apiClient = axios.create({
-    baseURL: 'http://127.0.0.1:8000/api/v1',
+    baseURL: isRunningInDocker() ? "http://backend:8000/api/v1" : "http://127.0.0.1:8000/api/v1",
     headers: {
         'Content-Type': 'application/json',
         'Authorization': `Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0b2tlbl90eXBlIjoiYWNjZXNzIiwiZXhwIjoxNzM5NDk0NDI1LCJpYXQiOjE3MzY5MDI0MjUsImp0aSI6ImRiYTU4MjIxMzAxODRmNzM4ZWU4ZGJhNzhlMjRlYThhIiwidXNlcl9pZCI6MTV9._ad09pNrxlyRsNeOPecl-hLb0R5C-q3aeXl7D04MSBM`,
